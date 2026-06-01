e12d9fe88564209fbc11fd4359d95580ff11cba4
M-Y-YAKOUT <moheda438@gmail.com>
2026-06-01 06:15:10 +0500

    fix system Repayment , make same size for right side

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const clearValue = document.querySelector('#clearValue');
 const mortgageValue = document.querySelector('#mortgageValue');
 const mortgageTerm = document.querySelector('#mortgageTerm');
 const interestRate = document.querySelector('#interestRate');
-
 const mortgageType = document.querySelectorAll('input[name="mortgageType"]')
-
 const calculate = document.querySelector('#calculate');
+const monthlyRepaymentsText = document.querySelector('.moneyCalc h1')
+const totalRepaymentsText = document.querySelector('.moneyCalc h3')
 
 clearValue.onclick = () => {
     mortgageValue.value = '';
@@ -23,13 +23,16 @@ calculate.addEventListener('click', function () {
 mortgageType.forEach(radio => {
     radio.addEventListener('change', function () {
         if (radio.value == 'Repayment') {
-            let result;
+            let monthlyRepayments;
+            let totalRepayments;
             let p = Number(mortgageValue.value);
             let n = Number(mortgageTerm.value * 12);
-            let r = Number((interestRate.value / 100) * 12);
+            let r = Number((interestRate.value / 100) / 12);
 
-            result = p * (r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1)
-            console.log(result)
+            monthlyRepayments = p * (r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1)
+            totalRepayments = monthlyRepayments * n
+            monthlyRepaymentsText.innerHTML = '£ ' + monthlyRepayments.toFixed(2);
+            totalRepaymentsText.innerHTML = '£ ' + totalRepayments.toFixed(2)
         } else if (radio.value == 'Interest') {
 
         }
